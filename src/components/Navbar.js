1939a9cb8b88ca9d710e11e63ca9bc87bc40ddfd
Ritik <rajatraj9559@gmail.com>
2021-12-24 10:37:02 +0530

    Made the navbar stick to top while scrolling

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,55 +1,55 @@
-import React, { Component } from 'react'
+import React, { Component } from 'react';
+import {
+    Link
+} from "react-router-dom";
 
 export class Navbar extends Component {
     render() {
         return (
-            <div>
+            <div className="sticky-top">
                 <nav className="navbar navbar-expand-lg navbar-dark bg-dark">
                     <div className="container-fluid">
-                        <a className="navbar-brand" href="/">Newsify</a>
+                        <Link className="navbar-brand" to="/">Newsify</Link>
                         <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
                             <span className="navbar-toggler-icon"></span>
                         </button>
                         <div className="collapse navbar-collapse" id="navbarSupportedContent">
                             <ul className="navbar-nav me-auto mb-2 mb-lg-0">
                                 <li className="nav-item">
-                                    <a className="nav-link active" aria-current="page" href="/">Home</a>
+                                    <Link className="nav-link" aria-current="page" to="/">Home</Link>
                                 </li>
                                 <li className="nav-item">
-                                    <a className="nav-link" href="/about">Business</a>
+                                    <Link className="nav-link" to="/business">Business</Link>
                                 </li>
                                 <li className="nav-item">
-                                    <a className="nav-link" href="/about">Sports</a>
+                                    <Link className="nav-link" to="/sports">Sports</Link>
                                 </li>
                                 <li className="nav-item">
-                                    <a className="nav-link" href="/about">National</a>
+                                    <Link className="nav-link" to="/national">National</Link>
                                 </li>
                                 <li className="nav-item">
-                                    <a className="nav-link" href="/about">World</a>
+                                    <Link className="nav-link" to="/world">World</Link>
                                 </li>
                                 <li className="nav-item">
-                                    <a className="nav-link" href="/about">Politics</a>
+                                    <Link className="nav-link" to="/politics">Politics</Link>
                                 </li>
                                 <li className="nav-item">
-                                    <a className="nav-link" href="/about">Technology</a>
+                                    <Link className="nav-link" to="/technology">Technology</Link>
                                 </li>
                                 <li className="nav-item">
-                                    <a className="nav-link" href="/about">Startup</a>
+                                    <Link className="nav-link" to="/startup">Startup</Link>
                                 </li>
                                 <li className="nav-item">
-                                    <a className="nav-link" href="/about">Entertainment</a>
+                                    <Link className="nav-link" to="/entertainment">Entertainment</Link>
                                 </li>
                                 <li className="nav-item">
-                                    <a className="nav-link" href="/about">Science</a>
+                                    <Link className="nav-link" to="/science">Science</Link>
                                 </li>
                                 <li className="nav-item">
-                                    <a className="nav-link" href="/about">Automobile</a>
+                                    <Link className="nav-link" to="/automobile">Automobile</Link>
                                 </li>
                                 <li className="nav-item">
-                                    <a className="nav-link" href="/about">Miscellaneous</a>
-                                </li>
-                                <li className="nav-item">
-                                    <a className="nav-link" href="/about">About</a>
+                                    <Link className="nav-link" to="/miscellaneous">Miscellaneous</Link>
                                 </li>
                             </ul>
                         </div>
